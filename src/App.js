81def45939a1c014336fc27390031baa37eb1379
nishthayadav456 <nishthayadav1122@gmail.com>
2023-07-17 23:13:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormClass from './Components/FormClass';
+
+function App() {
+  return (
+    <>
+    <h1>EMPLOYEE FEEDBACK FORM</h1>
+    <FormClass/>
+    </>
+  );
+}
+
+export default App;
